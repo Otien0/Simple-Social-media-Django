6e9adaf4e076681e15b2c1b71aff9bb9ca696e20
mourice-oduor <otienomaurice364@gmail.com>
2020-05-16 23:14:42 -0400

    Update groups/views.py file

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 from django.views import generic
 from django.shortcuts import get_object_or_404
 from groups.models import Group,GroupMember
+from . import models
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ("name", "description")
